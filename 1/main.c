21a221f5e9d752d8efc0b515bece904f4c0e03cf
Mark Thurston <mark@mdvthu.com>
2017-11-13 22:24:14 +0000

    Update Makefile

--- 1/main.c
+++ 1/main.c
@@ -4,8 +4,30 @@
 
 #define MAXLINELENGTH 1000
 
+/* convert a string formatted input line 
+ * into hexadecimal digits, stored in a 
+ * character array
+ * format for arguments: out = in */
+void processline(char *out, char *in)
+{
+}
+
+/* return the number of input lines */
+int getNinputlines() 
+{
+	int n;
+	printf("Enter number of input lines: ");
+	scanf("%d", &n);
+#ifdef DEBUG
+	printf("DEBUG: Call to getNinputlines(): User entered %d input lines\n", n);
+#endif
+	return n;
+}
+
 int main()
 {
+	/* number of input lines */
+	int n = getNinputlines();
 
 	return 0;
 }
