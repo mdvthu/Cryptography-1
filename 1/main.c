022148aa7b77fde78b92af3053ec1d40d6520d82
Mark Thurston <mark@mdvthu.com>
2017-11-16 21:22:39 +0000

    Input collection

--- 1/main.c
+++ 1/main.c
@@ -6,7 +6,7 @@
 #include "functions.h"
 
 /* maximum input line length */
-#define MAXLINELENGTH 1000
+#define MAXLINELENGTH (1000)
 
 
 /* Convert a string formatted input line into hexadecimal digits, stored in a 
@@ -33,23 +33,22 @@ int main()
 	/* prompt the user for the number of input lines */
 	int n = getNinputlines();
 
-	/* declare a generic counter variable */
-	int i;
-
 	/* buffer to store the string formatted line */
 	char *linebuf = malloc(MAXLINELENGTH + 1);
-
+	
 	/* create a structure for each line, holding line
 	 * and metadata including length of the line */
 	struct lines {
 		char *hexline;
 		unsigned int linelen;
 	} line[n];
-
-	for(i = 0; i < n; i++) {
+	
+	for(int i = 0; i < n; i++) {
 		printf("Input line %d: ", i + 1);
 		scanf("%s", linebuf);
+		line[i].linelen = processline(line[i].hexline, linebuf);
 	}
+	
 	printf("\n");
 
 	/* tidy up */
